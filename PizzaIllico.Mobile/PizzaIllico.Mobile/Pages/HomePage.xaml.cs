17f266bd57cbd7546ee7ed56b57db4a30f406cc3
Hubonhomme <Hubonhomme@users.noreply.github.com>
2021-03-28 20:29:22 +0200

    1st Push project

---
+++ PizzaIllico.Mobile/PizzaIllico.Mobile/Pages/HomePage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PizzaIllico.Mobile.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        private Boolean isConnected;
+        private StackLayout layoutNotConnected;
+        private StackLayout layoutConnected;
+        public HomePage()
+        {
+            BindingContext = new ViewModels.HomeViewModel();
+            InitializeComponent();
+            isConnected = false;
+            layoutNotConnected = this.FindByName("NotConnected") as StackLayout;
+            layoutConnected = this.FindByName("Connected") as StackLayout;
+            actualiseAffichage();
+        }
+        async void clickConnexion(object sender, EventArgs eventArgs)
+        {
+            await Navigation.PushAsync(new ConnexionPage());
+        }
+        async void clickCommande(object sender, EventArgs eventArgs)
+        {
+            await Navigation.PushAsync(new ShopListPage());
+        }
+        async void clickMap(object sender, EventArgs eventArgs)
+        {
+            await Navigation.PushAsync(new MapPage());
+        }
+        async void clickPanier(object sender, EventArgs eventArgs)
+        {
+            await Navigation.PushAsync(new ShopListPage());
+        }
+        async void clickAncienneCommande(object sender, EventArgs eventArgs)
+        {
+            await Navigation.PushAsync(new OrderPage());
+        }
+        async void clickCompte(object sender, EventArgs eventArgs)
+        {
+            await Navigation.PushAsync(new UserPage());
+        }
+        async void clickEnregistrement(object sender, EventArgs eventArgs)
+        {
+            await Navigation.PushAsync(new InscriptionPage());
+        }
+        private void clickSwitch(object sender, EventArgs e)
+        {
+            this.isConnected=!(this.isConnected);
+            actualiseAffichage();
+        }
+        private void actualiseAffichage()
+        {
+            this.layoutConnected.IsVisible=this.isConnected;
+            this.layoutNotConnected.IsVisible=!(this.isConnected);
+        }
+    }
+}
---
+++ PizzaIllico.Mobile/PizzaIllico.Mobile/ViewModels/MapViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using PizzaIllico.Mobile.Controls;
+using PizzaIllico.Mobile.Dtos;
+using PizzaIllico.Mobile.Dtos.Pizzas;
+using PizzaIllico.Mobile.Pages;
+using PizzaIllico.Mobile.Services;
+using Storm.Mvvm;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace PizzaIllico.Mobile.ViewModels
+{
+    public class MapViewModel : ViewModelBase
+    {
+        private ObservableCollection<ShopItem> _shops;
+        public INavigation Navigation { get;}
+        public ICommand SelectedCommand { get; }
+        public ObservableCollection<ShopItem> Shops
+        {
+            get => _shops;
+            set => SetProperty(ref _shops, value);
+        }
+
+        private Map _map;
+
+        public Map Map
+        {
+            get => _map;
+            set => SetProperty(ref _map, value); 
+        }
+        public MapViewModel(INavigation navigation)
+        {
+            Navigation = navigation;
+            SelectedCommand = new Command<ShopItem>(SelectedAction);
+            Map = new Map {MapType = 0, IsShowingUser = true};
+        }
+
+        private async void SelectedAction(ShopItem obj)
+        {
+            await Navigation.PushAsync(new PizzaListPage(obj.Id));
+        }
+
+        public override async Task OnResume()
+        {
+            await base.OnResume();
+
+            IPizzaApiService service = DependencyService.Get<IPizzaApiService>();
+
+            Response<List<ShopItem>> response = await service.ListShops();
+
+            Console.WriteLine($"Appel HTTP : {response.IsSuccess}");
+            if (response.IsSuccess)
+            {
+                Console.WriteLine($"Appel HTTP : {response.Data.Count}");
+                Shops = new ObservableCollection<ShopItem>(response.Data);
+                updateMap();
+            }
+        }
+
+        private void updateMap()
+        {
+            Map.Pins.Clear();
+            foreach (ShopItem shop in Shops)
+            {
+                Map.Pins.Add(new BindablePin
+                {
+                    Label = shop.Name,
+                    Address = shop.Address,
+                    Position = new Position(shop.Latitude,shop.Longitude),
+                    Type = PinType.Place
+                });
+            }
+        }
+    }
+}
